c67b0186ffafe8c7f2c9aca3773d872efbb3ef79
ijuresa <ivanjuresa611@gmail.com>
2020-04-17 22:40:11 +0200

    MASTER: Added RCC header

--- drv/drv_rcc.h
+++ drv/drv_rcc.h
@@ -122,6 +122,23 @@ typedef enum DRV_RCC_ahb_apb_ENUM {
     RCC_ahb_apb_COUNT = 55u
 } DRV_RCC_ahb_apb_E;
 
+//! MSI frequency ranges
+typedef enum DRV_RCC_MSI_freq_ENUM {
+    RCC_MSI_freq_100kHz = 0u,
+    RCC_MSI_freq_200kHz = 1u,
+    RCC_MSI_freq_400kHz = 2u,
+    RCC_MSI_freq_800kHz = 3u,
+    RCC_MSI_freq_1MHz   = 4u,
+    RCC_MSI_freq_2MHz   = 5u,
+    RCC_MSI_freq_4MHz   = 6u,
+    RCC_MSI_freq_8MHz   = 7u,
+    RCC_MSI_freq_16MHz  = 8u,
+    RCC_MSI_freq_24MHz  = 9u,
+    RCC_MSI_freq_32MHz  = 10u,
+    RCC_MSI_freq_48MHz  = 11u,
+    RCC_MSI_freq_COUNT  = 12u
+} DRV_RCC_MSI_freq_E;
+
 //! System clock candidates
 typedef enum DRV_RCC_sysClk_ENUM {
     RCC_sysClk_HSI_16 = 0u,
@@ -131,6 +148,77 @@ typedef enum DRV_RCC_sysClk_ENUM {
     RCC_sysClk_COUNT  = 4u
 } DRV_RCC_sysClk_E;
 
+//! APB division factors. HCLK is divided by it
+typedef enum DRV_RCC_APB_prescaler_ENUM {
+    RCC_APB_prescaler_1     = 0u,
+    RCC_APB_prescaler_2     = 1u,
+    RCC_APB_prescaler_4     = 2u,
+    RCC_APB_prescaler_8     = 3u,
+    RCC_APB_prescaler_16    = 4u,
+    RCC_APB_prescaler_COUNT = 5u
+} DRV_RCC_APB_prescaler_E;
+
+//! AHB division factors. SYSCLK is divided by it
+typedef enum DRV_RCC_AHB_prescaler_ENUM {
+    RCC_AHB_prescaler_1     = 0u,
+    RCC_AHB_prescaler_2     = 1u,
+    RCC_AHB_prescaler_4     = 2u,
+    RCC_AHB_prescaler_8     = 3u,
+    RCC_AHB_prescaler_16    = 4u,
+    RCC_AHB_prescaler_64    = 5u,
+    RCC_AHB_prescaler_128   = 6u,
+    RCC_AHB_prescaler_256   = 7u,
+    RCC_AHB_prescaler_512   = 8u,
+    RCC_AHB_prescaler_COUNT = 9u
+} DRV_RCC_AHB_prescaler_E;
+
+/** PLL Configuration **/
+//! PPL input clock source
+typedef enum DRV_RCC_PLL_inputClock_ENUM {
+    RCC_PLL_inputClock_MSI    = 0u,
+    RCC_PLL_inputClock_HSI_16 = 1u,
+    RCC_PLL_inputClock_HSE    = 2u,
+    RCC_PLL_inputClock_COUNT  = 3u
+} DRV_RCC_PLL_inputClock_E;
+
+//! Main PLL division factor for PLLCLK (System Clock)
+typedef enum DRV_RCC_PLL_r_ENUM {
+    RCC_PLL_r_2     = 0u,
+    RCC_PLL_r_4     = 1u,
+    RCC_PLL_r_6     = 2u,
+    RCC_PLL_r_8     = 3u,
+    RCC_PLL_r_COUNT = 4u,
+} DRV_RCC_PLL_r_E;
+
+//! Main PLL division factor for PLL48M1CLK (48MHz clock)
+typedef enum DRV_RCC_PLL_q_ENUM {
+    RCC_PLL_q_2     = 0u,
+    RCC_PLL_q_4     = 1u,
+    RCC_PLL_q_6     = 2u,
+    RCC_PLL_q_8     = 3u,
+    RCC_PLL_q_COUNT = 4u,
+} DRV_RCC_PLL_q_E;
+
+//! Main PLL division factor for PLLSAI3CLK (SAI1 and SAI2 clock)
+typedef enum DRV_RCC_PLL_p_ENUM {
+    RCC_PLL_p_7     = 0u,
+    RCC_PLL_p_17    = 1u,
+    RCC_PLL_p_COUNT = 2u
+} DRV_RCC_PLL_p_E;
+
+//! Division factor for PPL, SAI1 and SAI2 clocks. It will divide input clock before VCO calculation.
+typedef enum DRV_RCC_PLL_m_ENUM {
+    RCC_PLL_m_1     = 0u,
+    RCC_PLL_m_2     = 1u,
+    RCC_PLL_m_3     = 2u,
+    RCC_PLL_m_4     = 3u,
+    RCC_PLL_m_5     = 4u,
+    RCC_PLL_m_6     = 5u,
+    RCC_PLL_m_7     = 6u,
+    RCC_PLL_m_8     = 7u,
+    RCC_PLL_m_COUNT = 8u
+} DRV_RCC_PLL_m_E;
+
 /***************************************************************************************************
  *                      DATA TYPES
  **************************************************************************************************/
@@ -138,9 +226,27 @@ typedef enum DRV_RCC_sysClk_ENUM {
 /***************************************************************************************************
  *                      DATA STRUCTURES
  **************************************************************************************************/
+//! PPL Configuration structure
+typedef struct DRV_RCC_PLL_config_STRUCT {
+    uint8_t inputClock; //!< PLL input clock ::DRV_RCC_PLL_inputClock_E
+    uint8_t pllClk_R; //!< PLL System clock division ::DRV_RCC_PLL_r_E
+    uint8_t pllClk48_Q; //!< PLL 48MHz clock division ::DRV_RCC_PLL_q_E
+    bool_t is48ClkUsed; //!< Flag indicating if 48MHz clock will even be used. It can be turned OFF
+                        //!  to save power. It is used for USB, RNG and SDMMC (SD/SDIO/MMC)
+    uint8_t pllClkSai_P; //!< PLL SAI1 and SAI2 output division factor
+    bool_t isSaiClkUsed; //!< Flag indicating if (max) 80MHz clock will even be used. As 48MHz clock
+                         //!  it can be turned OFF to save power.
+    uint8_t pllClk_N; //!< Main VCO multiplication factor. (8 - 86 are valid values). VCO output
+                      //!  frequency should be between 64 and 344MHz
+} DRV_RCC_PLL_config_S;
+
+
 //! RCC Configuration structure
 typedef struct DRV_RCC_config_STRUCT {
     uint8_t systemClockSrc; //!< Chosen clock for System Clock ::DRV_RCC_sysClk_E
+    uint8_t prescalerApb1; //!< ::DRV_RCC_APB_prescaler_E
+    uint8_t prescalerApb2; //!< ::DRV_RCC_APB_prescaler_E
+    uint8_t prescalerAhb; //!< ::DRV_RCC_AHB_prescaler_E
 } DRV_RCC_config_S;
 
 /***************************************************************************************************
@@ -154,8 +260,11 @@ typedef struct DRV_RCC_config_STRUCT {
  * @brief   Function is used to reset input AHB or APB peripheral to its default state.
  * @details Input peripheral will reset its status to the same state as when MCU boots up.
  * *************************************************************************************************
- * @param   [in]      inPeripheral - Input AHB/APB peripheral ID ::DRV_RCC_ahb_apb_E
- * @param   [out]     outErr       - Output error enumerator
+ * @param   [in]       inPeripheral - Input AHB/APB peripheral ID ::DRV_RCC_ahb_apb_E
+ * @param   [out]     *outErr       - Output driver error enumerator
+ * *************************************************************************************************
+ * @exceptions        ERROR_err_ARGS_OUT_OF_RANGE: Input peripheral does not exist, out of boundaries
+ *                                                 or reset for that peripheral is not supported.
  * *************************************************************************************************
  * @return  Nothing
  **************************************************************************************************/
@@ -168,9 +277,12 @@ void DRV_RCC_peripheralReset(uint8_t inPeripheral, DRV_ERROR_err_E *outErr);
  *          After the enable bit is set, there is a 2 clock cycles delay before the clock is
  *          active. It means that SW will also need to wait for a delay. (Not a problem here)
  * *************************************************************************************************
- * @param   [in]      inPeripheral - Input AHB/APB peripheral ID ::DRV_RCC_ahb_apb_E
- * @param   [in]      inVal        - Disable (FALSE) or Enable (TRUE) Clock
- * @param   [out]     outErr       - Output error enumerator
+ * @param   [in]       inPeripheral - Input AHB/APB peripheral ID ::DRV_RCC_ahb_apb_E
+ * @param   [in]       inVal        - Disable (FALSE) or Enable (TRUE) Clock
+ * @param   [out]     *outErr       - Output driver error enumerator
+ * *************************************************************************************************
+ * @exceptions        ERROR_err_ARGS_OUT_OF_RANGE: Input peripheral does not exist, out of boundaries
+ *                                                 or reset for that peripheral is not supported.
  * *************************************************************************************************
  * @return  Nothing
  **************************************************************************************************/
